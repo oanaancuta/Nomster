bc245a5d9efdec7cf29f0b4e6ddc8cc04b877854
Oana Blankenship <ancutadraguta20@gmail.com>
2019-11-23 02:01:25 +0000

    Nomster 14 - Enable Create button on form and redirect user to root page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -8,5 +8,21 @@ class PlacesController < ApplicationController
     @place = Place.new
   end
 
+  
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
+
+  private
+
+  
+  def place_params
+    params.require(:place).permit(:name, :address, :description)
+  end
+
 
 end
+
+# Lines 12 - 15 are used to store place in the database and redirect user to places controller, index action - root Prefix (using rake routes command) => send user to root_path
+# Lines 20 - 22 are used to pull the user provided name, address, description values from the form
