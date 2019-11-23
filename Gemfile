247c824150dcd460884f304309e1eb1473730bb1
Oana Blankenship <ancutadraguta20@gmail.com>
2019-11-23 00:50:29 +0000

    Nomster 13 - added form for user input

--- Gemfile
+++ Gemfile
@@ -69,3 +69,8 @@ gem 'bootstrap', '~> 4.0.0.alpha6'
 source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
 end
+
+gem 'will_paginate', '~> 3.2.1'
+gem 'will_paginate-bootstrap'
+
+gem 'simple_form'
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,12 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
+    @places = Place.all.paginate(:page => params[:page], :per_page => 3)
   end
+
+  def new
+    @place = Place.new
+  end
+
+
 end
